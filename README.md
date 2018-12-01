c3354d2d1be76040926f13d2a60bfb92353119d1
enigmaeth <enigmaeth@gmail.com>
2018-12-01 15:11:51 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+### React Crypto Currency Ticker
+
+  React based front-end for real time crypto-currency price using Axios fetch.
+
+### Requirements
+
+To install the requirements, run:
+
+```
+$ npm install
+```
+
+This will install the packages from the  `package.json` file.
+
+To start the development server, run:
+``` 
+$ npm start
+```
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import cryptoApp from './reducers';
+import registerServiceWorker from './registerServiceWorker';
+
+
+const store = createStore(cryptoApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+// const store = createStore(cryptoApp);
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, document.getElementById('root'));
+
+registerServiceWorker();
